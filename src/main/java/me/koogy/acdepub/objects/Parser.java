9b3d8e9c471b8928e2af9643aec0b9bf77497738
adean <git@koogy.clara.co.uk>
2014-01-30 22:30:47 +0000

    Changed to use Dom. Bugfixes based on generated files. Indented paragraphs.

---
+++ src/main/java/me/koogy/acdepub/objects/Parser.java
@@ -0,0 +1,131 @@
+package me.koogy.acdepub.objects;
+
+import java.util.ArrayList;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * Dom parsing code. Not needed for jaxb (if that worked)
+ * 
+ * @author adean
+ */
+public class Parser {
+    
+    public static void parseBook(Book book, Document doc) {
+        parseInfo(book, doc);
+        parsePrefix(book, doc);
+        parseParts(book, doc);
+        parseChapters(book, doc);
+        parseAppendix(book, doc);
+    }
+    
+    public static void parseInfo(Book book, Document doc) {
+        
+        NodeList nodeList = doc.getElementsByTagName(Tag.INFO).item(0).getChildNodes();
+        for (int i = 0 ; i < nodeList.getLength() ; i++) {
+            Node node = nodeList.item(i);
+            String name = node.getNodeName();
+            String value = node.getTextContent();
+            if (name.equalsIgnoreCase(Tag.TITLE)) {
+                book.setTitle(value);
+            }
+            if (name.equalsIgnoreCase(Tag.AUTHOR)) {
+                book.setAuthor(value);
+            }
+            if (name.equalsIgnoreCase(Tag.DATE)) {
+                book.setDate(value);
+            }
+        }
+    }
+
+    // these are chapters
+    public static void parsePrefix(Book book, Document doc) {
+        
+        NodeList nodeList = doc.getElementsByTagName(Tag.PREFIX);
+        if (nodeList.getLength() != 0) {
+            book.setPrefaces(new ArrayList<GenericChapter>());
+            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+                Node node = nodeList.item(i);
+                Chapter chapter = parseChapter(node);
+                book.getPrefaces().add(chapter);
+            }
+        }
+    }
+
+    // these are chapters (not in parts)
+    // TODO how to distinguish between the two?
+    public static void parseChapters(Book book, Document doc) {
+        
+        NodeList nodeList = doc.getElementsByTagName(Tag.CHAPTER);
+        if (nodeList.getLength() != 0) {
+            book.setChapters(new ArrayList<GenericChapter>());
+            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+                Node node = nodeList.item(i);
+                Chapter chapter = parseChapter(node);
+                book.getChapters().add(chapter);
+            }
+        }
+    }
+
+    // Parse parts, each of which can contain title and chapters
+    public static void parseParts(Book book, Document doc) {
+        
+        NodeList partNodeList = doc.getElementsByTagName(Tag.PART);
+        if (partNodeList.getLength() != 0) {
+            book.setParts(new ArrayList<Part>());
+            for (int i = 0 ; i < partNodeList.getLength() ; i++) {
+                Node partNode = partNodeList.item(i);
+                Part part = new Part();
+                part.setChapters(new ArrayList<GenericChapter>());
+                NodeList childList = partNode.getChildNodes();
+                for (int j = 0 ; j < childList.getLength() ; j++) {
+                    Node childNode = childList.item(j);
+                    String name = childNode.getNodeName();
+                    if (name.equalsIgnoreCase(Tag.TITLE)) {
+                        part.setTitle(childNode.getTextContent());
+                    }
+                    if (name.equalsIgnoreCase(Tag.CHAPTER)) {
+                        Chapter chapter = parseChapter(childNode);
+                        part.getChapters().add(chapter);
+                    }
+                }
+                book.getParts().add(part);
+            }
+        }
+    }
+
+    // these are chapters
+    public static void parseAppendix(Book book, Document doc) {
+        
+        NodeList nodeList = doc.getElementsByTagName(Tag.APPENDIX);
+        if (nodeList.getLength() != 0) {
+            book.setAppendices(new ArrayList<GenericChapter>());
+            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+                Node node = nodeList.item(i);
+                Chapter chapter = parseChapter(node);
+                book.getAppendices().add(chapter);
+            }
+        }
+    }
+
+    private static Chapter parseChapter(Node chapterNode) {
+        Chapter chapter = new Chapter();
+        NodeList nodeList = chapterNode.getChildNodes();
+        if (nodeList.getLength() != 0) {
+            chapter.setParas(new ArrayList<String>());
+            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+                Node node = nodeList.item(i);
+                String name = node.getNodeName();
+                String value = node.getTextContent();
+                if (name.equalsIgnoreCase(Tag.PARA)) {
+                    chapter.getParas().add(value);
+                }
+                if (name.equals(Tag.TITLE)) {
+                    chapter.setTitle(value);
+                }
+            }
+        }
+        return chapter;
+    }
+}
---
+++ src/main/java/me/koogy/acdepub/objects/Tag.java
@@ -0,0 +1,18 @@
+package me.koogy.acdepub.objects;
+
+/**
+ * @author adean
+ */
+public class Tag {
+    public static final String BOOK     = "book";
+    public static final String INFO     = "info";
+    public static final String PREFIX   = "prefix";
+    public static final String PART     = "part";
+    public static final String CHAPTER  = "chapter";
+    public static final String PARA     = "p";
+    public static final String APPENDIX = "appendix";
+    
+    public static final String TITLE    = "title";
+    public static final String AUTHOR   = "author";
+    public static final String DATE     = "date";
+}
