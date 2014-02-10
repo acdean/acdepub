07ede9ffec475a4b86f3ab3281b4e25814524b36
adean <git@koogy.clara.co.uk>
2014-02-10 22:31:58 +0000

    deprecated

--- src/main/java/me/koogy/acdepub/objects/GenericParagraph.java
+++ src/main/java/me/koogy/acdepub/objects/GenericParagraph.java
@@ -6,6 +6,7 @@ import javax.xml.bind.annotation.XmlElement;
  * As per html paragraph. Can contain <em> and &emdash; etc
  * @author adean
  */
+@Deprecated
 public class GenericParagraph {
     String contents;
 
