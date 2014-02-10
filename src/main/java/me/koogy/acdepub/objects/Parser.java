98a5124db32f8b619e605fbca07e831257bd1ce1
adean <git@koogy.clara.co.uk>
2014-02-10 22:25:09 +0000

    Deprecated DOM parser

--- src/main/java/me/koogy/acdepub/objects/Parser.java
+++ src/main/java/me/koogy/acdepub/objects/Parser.java
@@ -12,6 +12,7 @@ import org.w3c.dom.NodeList;
  * 
  * @author adean
  */
+@Deprecated
 public class Parser {
     
     private static Logger log = LogManager.getLogger(Parser.class);
@@ -37,13 +38,13 @@ public class Parser {
             String name = node.getNodeName();
             String value = node.getTextContent();
             if (name.equalsIgnoreCase(Tag.TITLE)) {
-                book.setTitle(value);
+                book.getInfo().setTitle(value);
             }
             if (name.equalsIgnoreCase(Tag.AUTHOR)) {
-                book.setAuthor(value);
+                book.getInfo().setAuthor(value);
             }
             if (name.equalsIgnoreCase(Tag.DATE)) {
-                book.setDate(value);
+                book.getInfo().setDate(value);
             }
             if (name.equalsIgnoreCase(Tag.OPTION)) {
                 Options.parseOption(book, node);
@@ -55,16 +56,16 @@ public class Parser {
     public static void parsePrefix(Book book, Document doc) {
         log.info("parsePrefix");
         
-        NodeList nodeList = doc.getElementsByTagName(Tag.PREFIX);
-        if (nodeList.getLength() != 0) {
-            book.setPrefaces(new ArrayList<GenericChapter>());
-            for (int i = 0 ; i < nodeList.getLength() ; i++) {
-                Node node = nodeList.item(i);
-                Chapter chapter = parseChapter(node);
-                chapter.setType(GenericChapter.PREFIX);
-                book.getPrefaces().add(chapter);
-            }
-        }
+//        NodeList nodeList = doc.getElementsByTagName(Tag.PREFIX);
+//        if (nodeList.getLength() != 0) {
+//            book.setPrefaces(new ArrayList<Preface>());
+//            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+//                Node node = nodeList.item(i);
+//                Preface preface = (Preface)parseChapter(node);
+//                preface.setType(GenericChapter.PREFIX);
+//                book.getPrefaces().add(preface);
+//            }
+//        }
     }
 
     // these are chapters (not in parts)
@@ -72,64 +73,64 @@ public class Parser {
     public static void parseChapters(Book book, Document doc) {
         log.info("parseChapters");
         
-        NodeList nodeList = doc.getElementsByTagName(Tag.CHAPTER);
-        if (nodeList.getLength() != 0) {
-            book.setChapters(new ArrayList<GenericChapter>());
-            for (int i = 0 ; i < nodeList.getLength() ; i++) {
-                Node node = nodeList.item(i);
-                Chapter chapter = parseChapter(node);
-                chapter.setType(GenericChapter.CHAPTER);
-                book.getChapters().add(chapter);
-            }
-        }
+//        NodeList nodeList = doc.getElementsByTagName(Tag.CHAPTER);
+//        if (nodeList.getLength() != 0) {
+//            book.setChapters(new ArrayList<Chapter>());
+//            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+//                Node node = nodeList.item(i);
+//                Chapter chapter = (Chapter)parseChapter(node);
+//                chapter.setType(GenericChapter.CHAPTER);
+//                book.getChapters().add(chapter);
+//            }
+//        }
     }
 
     // Parse parts, each of which can contain title and chapters
     public static void parseParts(Book book, Document doc) {
         log.info("parseParts");
-        
-        NodeList partNodeList = doc.getElementsByTagName(Tag.PART);
-        if (partNodeList.getLength() != 0) {
-            book.setParts(new ArrayList<Part>());
-            for (int i = 0 ; i < partNodeList.getLength() ; i++) {
-                Node partNode = partNodeList.item(i);
-                Part part = new Part();
-                part.setChapters(new ArrayList<GenericChapter>());
-                NodeList childList = partNode.getChildNodes();
-                for (int j = 0 ; j < childList.getLength() ; j++) {
-                    Node childNode = childList.item(j);
-                    String name = childNode.getNodeName();
-                    if (name.equalsIgnoreCase(Tag.TITLE)) {
-                        part.setTitle(childNode.getTextContent());
-                    }
-                    if (name.equalsIgnoreCase(Tag.CHAPTER)) {
-                        Chapter chapter = parseChapter(childNode);
-                        chapter.setType(GenericChapter.PART_CHAPTER);
-                        part.getChapters().add(chapter);
-                    }
-                }
-                book.getParts().add(part);
-            }
-        }
+//        
+//        NodeList partNodeList = doc.getElementsByTagName(Tag.PART);
+//        if (partNodeList.getLength() != 0) {
+//            book.setParts(new ArrayList<Part>());
+//            for (int i = 0 ; i < partNodeList.getLength() ; i++) {
+//                Node partNode = partNodeList.item(i);
+//                Part part = new Part();
+//                part.setChapters(new ArrayList<Chapter>());
+//                NodeList childList = partNode.getChildNodes();
+//                for (int j = 0 ; j < childList.getLength() ; j++) {
+//                    Node childNode = childList.item(j);
+//                    String name = childNode.getNodeName();
+//                    if (name.equalsIgnoreCase(Tag.TITLE)) {
+//                        part.setTitle(childNode.getTextContent());
+//                    }
+//                    if (name.equalsIgnoreCase(Tag.CHAPTER)) {
+//                        Chapter chapter = (Chapter)parseChapter(childNode);
+//                        chapter.setType(GenericChapter.PART_CHAPTER);
+//                        part.getChapters().add(chapter);
+//                    }
+//                }
+//                book.getParts().add(part);
+//            }
+//        }
     }
 
     // these are chapters
     public static void parseAppendix(Book book, Document doc) {
         log.info("parseAppendix");
         
-        NodeList nodeList = doc.getElementsByTagName(Tag.APPENDIX);
-        if (nodeList.getLength() != 0) {
-            book.setAppendices(new ArrayList<GenericChapter>());
-            for (int i = 0 ; i < nodeList.getLength() ; i++) {
-                Node node = nodeList.item(i);
-                Chapter chapter = parseChapter(node);
-                chapter.setType(GenericChapter.APPENDIX);
-                book.getAppendices().add(chapter);
-            }
-        }
+//        NodeList nodeList = doc.getElementsByTagName(Tag.APPENDIX);
+//        if (nodeList.getLength() != 0) {
+//            book.setAppendices(new ArrayList<Appendix>());
+//            for (int i = 0 ; i < nodeList.getLength() ; i++) {
+//                Node node = nodeList.item(i);
+//                Appendix appendix = (Appendix)parseChapter(node);
+//                appendix.setType(GenericChapter.APPENDIX);
+//                book.getAppendices().add(appendix);
+//            }
+//        }
     }
 
-    private static Chapter parseChapter(Node chapterNode) {
+    private static GenericChapter parseChapter(Node chapterNode) {
         log.info("parseChapter");
         Chapter chapter = new Chapter();
         NodeList nodeList = chapterNode.getChildNodes();
